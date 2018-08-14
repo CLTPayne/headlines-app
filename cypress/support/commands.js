3a801b4aef49b0726b770ae38b4bcdb4b6fe86c7
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-14 23:57:46 +0100

    Abstract repeat cypress calls as custom commands
    * Avoid repeated code and improve test speed

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,11 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+const exampleMock = require('../fixtures/headlinesPageOne.json');
+
+Cypress.Commands.add('useMocks', function() {
+  cy.server({ matchBase: false })
+  cy.route(/api.ft/, exampleMock)
+});
+
+Cypress.Commands.add('searchWord', function() {
+  cy.get('form input:first').type('Brexit')
+  cy.get('form input:second').click()
+})
