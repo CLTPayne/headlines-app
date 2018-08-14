aeb6323cfe673405228cab68afd27285fe247aec
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-14 14:16:17 +0100

    Initialize cypress for feature testing
    * Add testers to ensure set up is fassing and failing correctly

---
+++ cypress/integration/initialization.spec.js
@@ -0,0 +1,9 @@
+describe('Check initial setup', function() {
+  it('confirms that Cypress is installed and passes a test', function() {
+    expect(true).to.equal(true)
+  })
+
+  it('confirms that Cypress is installed and fails a test', function() {
+    expect(true).to.equal(false)
+  })
+})
