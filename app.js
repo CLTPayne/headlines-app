7b8291ebc60491391471024ebe76d5c1e289fe5b
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-14 11:48:41 +0100

    Add 404 handler and static files middleware to app

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const PORT = 3000;
 
+app.use(express.static('public'))
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
@@ -10,4 +12,7 @@ app.get('/', function(req, res) {
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}!`)
 });
- 
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry we're not sure that page exists?")
+})
