9989e30377b0584c747bba0098c283b2cef60b91
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-15 15:05:37 +0100

    Update app to use views and add run index route

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const index = require('./routes/index');
 const PORT = 3000;
 
-app.use(express.static('public'))
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
 
 app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}!`)
+  console.log(`App is listening on port ${PORT}!`);
 });
 
 app.use(function (req, res, next) {
-  res.status(404).send("Sorry we're not sure that page exists?")
-})
+  res.status(404).send("Sorry we're not sure that page exists?");
+});
