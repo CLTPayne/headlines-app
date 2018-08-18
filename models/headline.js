60cdc247925cf20578d72c17284edf7fb6ce2800
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-18 15:41:17 +0100

    Fix missing image for headlines error
    * Error also thrown by empty array of images in API response object

--- models/headline.js
+++ models/headline.js
@@ -21,8 +21,10 @@ Headline.prototype.theme = function() {
 };
 
 Headline.prototype.image = function() {
-  if (this.headline.images !== undefined) {
+  if (this.headline.images.length !== 0) {
     return this.headline.images[0].url
+  } else if (this.headline.images === undefined) {
+    return null
   } else {
     return null
   }
