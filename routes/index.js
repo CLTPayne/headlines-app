d019245b1da0612ec32ac32d7f5f7dfd314aff1b
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-18 13:49:31 +0100

    Add external API call from index route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,39 @@
-const express = require('express');
-const router = express.Router();
+var express = require("express");
+var router = express.Router();
+var request = require("request");
+var bodyParser =  require("body-parser");
+var HeadlineList = require("../models/headlineList");
 
-router.get('/', function(req, res, next) {
-  res.render('index')
+router.use(bodyParser.json());
+
+router.get("/", function(req, res) {
+  request.post({
+    "headers": {
+      "content-type": "application/json",
+      "X-Api-Key": process.env.API_KEY
+    },
+    "url": "http://api.ft.com/content/search/v1",
+    "body": JSON.stringify({
+      "queryString": "",
+      "resultContext" : {
+        "aspects" :[  "title","images","lifecycle", "metadata","editorial" ],
+        "sortOrder" : "DESC",
+        "sortField" : "initialPublishDateTime",
+        "maxResults" : "20",
+ 	      "offset" : "0"
+	    }
+    })
+  },
+  function(error, response, body, headlineList = new HeadlineList) {
+    if (error) {
+      return (error);
+    }
+    else {
+      var headlines = JSON.parse(body);
+      headlineList.addHeadlines(headlines);
+    }
+  });
+  res.render("index");
 });
 
 module.exports = router;
---
+++ spec/indexSpec.js
@@ -0,0 +1,63 @@
+"use strict";
+
+var dotenv = require('dotenv').config()
+var request = require('supertest')('http://api.ft.com/content/search/v1');
+var index = require('../routes/index');
+
+describe('Index route', function() {
+
+  describe("external API call to the FT", function() {
+
+    var search = ({
+      "queryString": "",
+      "resultContext" : {
+        "aspects" :[  "title","images","lifecycle", "metadata","editorial" ],
+        "sortOrder" : "DESC",
+        "sortField" : "initialPublishDateTime",
+        "maxResults" : "20",
+        "offset" : "0"
+      }
+    });
+
+    it('receives a 200 OK with a valid search', function(done) {
+      var search = ({
+        "queryString": "",
+        "resultContext" : {
+          "aspects" :[  "title","images","lifecycle", "metadata","editorial" ],
+          "sortOrder" : "DESC",
+          "sortField" : "initialPublishDateTime",
+          "maxResults" : "20",
+          "offset" : "0"
+        }
+      });
+      request.post('/')
+        .send(search)
+        .set('X-API-Key', process.env.API_KEY)
+        .expect(200, done);
+    });
+
+    it('receives a JSON object', function(done) {
+      request.post('/')
+        .send(search)
+        .set('X-API-Key', process.env.API_KEY)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', 'application/json;charset=UTF-8', done);
+    });
+
+    it('receives a response body containing headlines', function(done) {
+      request.post('/')
+        .send(search)
+        .set('X-API-Key', process.env.API_KEY)
+        .set('Accept', 'application/json')
+        .end(function(err, res){
+          if(err){
+            return done(err);
+          }
+          search = res.body;
+          done();
+      });
+    });
+
+  });
+
+});
