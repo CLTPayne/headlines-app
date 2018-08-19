ab290d083dd8f03e46299e478d9b1d16b193c1cd
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-19 18:26:12 +0100

    Fix cypress headlines spec as per final views
    * Follow BDD cycle to update feature tests as per HTML info

--- cypress/integration/headlines.spec.js
+++ cypress/integration/headlines.spec.js
@@ -1,27 +1,24 @@
 context('Top headlines', function() {
 
   beforeEach( function() {
-    cy.useMocks()
-    cy.visit('/')
-  })
+    cy.useMocks();
+    cy.visit('/');
+  });
 
-  it('displays a site heading', function() {
-    cy.contains('h1', 'Financial Headlines')
-  })
+  it('loads 20 headlines content blocks on page load', function() {
+    cy.get('article').should('have.length', 20);
+  });
 
-  it('loads the first 20 headlines', function() {
-    const headlineOne = 'Trump and the use and misuse of US sanctions'
-    const headlineTwo = 'De-risking agricultural investment in Africa'
-    cy.get('#headlines').contains(headlineOne).and(headlineTwo)
-  })
+  it('loads 20 headlines on page load', function() {
+    cy.get('.o-teaser__heading').should('have.length', 20);
+  });
 
-  it('displays a primary theme for a headline', function() {
-    cy.get('#headline-one').contains('US politics & policy')
-  })
+  it('loads 20 headline themes on page load', function() {
+    cy.get('.o-teaser__theme').should('have.length', 20);
+  });
 
-  it('displays a subheading for a headline', function() {
-    const subheadingOne = 'Penalties should be imposed judiciously and co-ordinated with allies'
-    cy.get('#headline-one').contains(subheadingOne)
-  })
+  it('displays 20 news summaries on page load', function() {
+    cy.get('.o-teaser__summary').should('have.length', 20);
+  });
 
-})
+});
