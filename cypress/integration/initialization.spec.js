aa1e1b18ae07e3cb4d62a1fa352e7f17ef7ac1c7
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-19 16:26:15 +0100

    Remove cypress initialisation test
    * Full test suite runs so initialissation is not needed

--- cypress/integration/initialization.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Check initial setup', function() {
-  it('confirms that Cypress is installed and passes a test', function() {
-    expect(true).to.equal(true)
-  })
-
-  it('confirms that Cypress is installed and fails a test', function() {
-    expect(true).to.equal(false)
-  })
-})
