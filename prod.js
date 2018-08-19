55019ca6b1265314d0b9aa127ef52dec8e6c4ddd
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-19 22:33:05 +0100

    Fix linting for code consistency

--- prod.js
+++ prod.js
@@ -1,7 +1,7 @@
-var helmet = require('helmet');
-var compression = require('compression');
+var helmet = require("helmet");
+var compression = require("compression");
 
 module.exports = function(app) {
   app.use(helmet());
   app.use(compression());
-}
+};
