25eab34c2cd7b9395e6bd4b8c3a1bf58c4a7476d
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-19 18:51:29 +0100

    Update cypress search feature tests as per views
    * Update page elements as per final view templates for full searchbar
    feature testing

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,11 +1,11 @@
 const exampleMock = require('../fixtures/headlinesPageOne.json');
 
 Cypress.Commands.add('useMocks', function() {
-  cy.server({ matchBase: false })
-  cy.route('POST', 'http://api.ft.com/content/search/v1', 'fixture:headlinesPageOne.json')
+  cy.server({ matchBase: false });
+  cy.route('POST', 'http://api.ft.com/content/search/v1', 'fixture:headlinesPageOne.json');
 });
 
 Cypress.Commands.add('searchWord', function() {
-  cy.get('form input:first').type('Brexit')
-  cy.get('form input:second').click()
-})
+  cy.get('form input:first').type('Brexit');
+  cy.get('.form-inline > .o-buttons').click();
+});
